d94c8e1097b8d8ade3b8d48ad8ce21d6c850e29e
eonpain <zmfltmwnsrl@naver.com>
2024-03-06 14:57:50 +0900

    Init : vscode setting.json 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <>
       <div>
-        <span className="text-reds-600 tablet:text-blue-600 pc:text-green-600">안녕하세요</span>
+        <span className="text-reds-600 tablet:text-blue-600 pc:text-green-600">
+          안녕하세요
+        </span>
       </div>
     </>
   );
