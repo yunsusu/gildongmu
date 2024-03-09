b6084fcd668912ba3cf88962e889b98378a31414
eonpain <zmfltmwnsrl@naver.com>
2024-03-10 01:25:06 +0900

    feat: 전역 폰트 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      "nanum-square-round": ["NanumSquareRound", "sans-serif"],
+    },
     container: {
       center: true,
       padding: "2rem",
