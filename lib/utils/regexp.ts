17b38f95aacc8bc4ca07e5ba491eb0e1729a2212
JunCastle <juncastle97@gmail.com>
2024-03-11 15:24:31 +0900

    feat : 유효성 함수 추가

---
+++ lib/utils/regexp.ts
@@ -0,0 +1,5 @@
+export const regEmail =
+  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+export const regPassword =
+  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
