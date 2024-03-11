2efd06882e77ea8b687dc9df2d930cb0d93177e9
bumsly <qjatmd2005@naver.com>
2024-03-11 18:50:47 +0900

    feat : "Shadcn Button 적용 & 간편로그인 이미지 넣기"

---
+++ pages/login.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export default function Login() {
+  return (
+    <>
+      <div className="flex">
+        <div className="bg-kakao w-1/2 h-screen text-50"></div>
+        <div className="bg-bg-06 w-1/2 h-screen text-14 flex justify-center items-center">
+          <div className="bg-white w-3/4 h-5/6 p-40 flex flex-col justify-between items-center rounded-32">
+            <h1 className="text-32 text-text-01 font-extrabold">로그인</h1>
+            <ul className="w-full">
+              <li className="w-full mb-32">
+                <input
+                  type="text"
+                  placeholder="이메일"
+                  className="bg-bg-02 w-full h-52 flex justify-end items-center self-stretch px-16 gap-8 rounded-xl"
+                />
+              </li>
+              <li className="w-full">
+                <input
+                  type="text"
+                  placeholder="비밀번호"
+                  className="bg-bg-02 w-full h-52 flex justify-end items-center self-stretch px-16 gap-8 rounded-xl"
+                />
+              </li>
+            </ul>
+            <div className="w-full text-18">
+              <Button className="bg-line-02 text-text-04 font-extrabold w-full h-52 mb-20 rounded-32">
+                로그인
+              </Button>
+              <Button className="bg-white border-[1.5px] border-primary font-extrabold text-primary w-full h-52 rounded-32">
+                회원 가입하기
+              </Button>
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <div className="border-b border-line-01 w-1/3"></div>
+              <p className="text-text-01 text-16 w-1/3 flex justify-center leading-[130%] tracking-[-0.6px]">
+                SNS 로그인
+              </p>
+              <div className="border-b border-line-01 w-1/3"></div>
+            </div>
+            <div className="w-full flex justify-between items-center text-18">
+              <Button className="bg-kakao mr-20 w-1/2 h-52 flex justify-center text-text-02 rounded-32">
+                <Image
+                  src="/icons/kakao.png"
+                  alt="kakao"
+                  width="32"
+                  height="32"
+                  className="pr-8"
+                />
+                카카오톡
+              </Button>
+              <Button className="bg-bg-02 w-1/2 h-52 flex justify-center text-text-02 rounded-32">
+                <Image
+                  src="/icons/google.png"
+                  alt="google"
+                  width="32"
+                  height="32"
+                  className="pr-8"
+                />
+                구글
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
