fb1ac388208ad55d05e5a1b6468ddff1c0d9d911
imkrmin <zum2n2@gmail.com>
2024-03-12 18:51:16 +0900

    fix : 메인페이지 반응형 코드 수정 및 아이콘 오류 수정

--- components/main/header/searchbar.tsx
+++ components/main/header/searchbar.tsx
@@ -2,22 +2,22 @@ import Image from "next/image";
 
 export default function Searchbar() {
   const placeholderStyle =
-    "placeholder:font-NanumSquareRound placeholder:text-18 placeholder:font-bold placeholder:leading-tight placeholder:tracking-tight mobile:placeholder:text-16";
+    "placeholder:text-18 placeholder:font-bold placeholder:leading-tight placeholder:tracking-tight tablet:placeholder:text-16";
 
   return (
     <>
       <div className="relative">
         <input
-          className={`flex w-[564px] tablet:w-[564px] mobile:w-312 h-60 tablet:h-52 mobile:h-52 pt-12 pb-12 pl-24 pr-12 rounded-32 bg-bg-01 ${placeholderStyle}`}
+          className={`flex w-[564px] tablet:w-[564px] mobile:w-312 h-60 tablet:h-52 pt-12 pb-12 pl-24 pr-12 rounded-32 bg-bg-01 tracking-tight leading-tight font-bold text-text-02 text-18 tablet:text-16 ${placeholderStyle}`}
           placeholder="가고 싶은 여행지를 검색해 주세요!"
-        ></input>
+        />
         <button>
           <Image
-            src={"/images/search.svg"}
+            src={"/icons/search.svg"}
             alt="돋보기 아이콘"
             width={24}
             height={24}
-            className="absolute top-15 right-20"
+            className="absolute top-17 tablet:top-15 right-20"
           />
         </button>
       </div>
