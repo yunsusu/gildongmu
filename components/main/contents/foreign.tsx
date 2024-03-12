2f3cf1dad2eaa7da963c34164e257a7ba2f958d2
imkrmin <zum2n2@gmail.com>
2024-03-12 15:31:45 +0900

    design : 메인페이지 국내여행, 해외여행 ui 구현

---
+++ components/main/contents/foreign.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Foreign() {
+  return (
+    <div className="flex flex-col self-stretch gap-40 px-24 py-60 tablet:py-40">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex justify-start gap-12">
+          <Image
+            src={"/icons/plane.svg"}
+            alt="비행기 아이콘"
+            width={40}
+            height={40}
+          />
+          <span className="font-extrabold leading-10 tracking-tight text-center text-text-01 text-32 tablet:text-24">
+            해외 여행
+          </span>
+        </div>
+        <div>버튼</div>
+      </div>
+      <div className="flex items-center justify-center w-full">
+        카드 컴포넌트
+      </div>
+    </div>
+  );
+}
