6471b6b384b35a6d904bed79fd92759fbff44a47
imkrmin <zum2n2@gmail.com>
2024-03-12 14:18:02 +0900

    design: 메인페이지 header ui 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
-import ContryCarousel from "@/components/carousel/country";
-import CustomCarousel from "@/components/carousel/custom";
+import Contents from "@/components/main/contents";
+import Header from "@/components/main/header";
 
-export default function Home() {
+export default function Main() {
   return (
     <>
-      <div>
-        <span className="text-primary text-8 tablet:text-red-600 mobile:text-blue-700">
-          안녕하세요
-        </span>
-        <div className="w-1200 p-20 flex flex-col justify-center">
-          <CustomCarousel />
-          <div className="h-30"></div>
-          <ContryCarousel />
-        </div>
+      <div className="min-h-screen">
+        <Header />
+        <Contents />
       </div>
     </>
   );
