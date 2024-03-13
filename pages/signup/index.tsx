bade029021cb5fa51b26ebb4727c65b32234039a
JunCastle <juncastle97@gmail.com>
2024-03-13 23:27:01 +0900

    fix : font weight 수정

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "@/components/form/signupForm";
 function Signup() {
   return (
     <div className="bg-bg-06 flex flex-col justify-start items-center pb-80">
-      <h1 className="h-120 text-32 text-text-01 flex items-center tablet:h-100 tablet:text-24">회원가입</h1>
+      <h1 className="h-120 text-32 font-extrabold text-text-01 flex items-center tablet:h-100 tablet:text-24">회원가입</h1>
       <SignUpForm />
     </div>
   );
