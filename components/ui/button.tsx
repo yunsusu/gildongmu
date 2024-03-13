c9f542892a4ad6e39a77b4b584049224d81d60e1
eonpain <zmfltmwnsrl@naver.com>
2024-03-13 21:06:26 +0900

    feat: shadcn/ui 버튼 컴포넌트를 수정

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,23 +5,22 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-line-02 disabled:text-text-04",
   {
-    // rounded-md text-sm 삭제(범승)
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary-press",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "text-primary border-[1.5px] border-primary bg-background hover:text-primary-press hover:border-primary-press",
+        secondary: "bg-secondary text-white hover:bg-secondary-press",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        error: "text-white bg-error hover:bg-rose-300",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "w-90 h-44 px-10 py-16 rounded-32 font-extrabold",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
