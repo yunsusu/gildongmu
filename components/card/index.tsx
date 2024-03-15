086f26d0ad015cc7ea861aba27ac3294bb18017b
yunsusu <rladbstn1212@gmail.com>
2024-03-15 18:13:03 +0900

    feat : mock으로 데이터 위치 잡기

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -2,17 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+const content = {
+  id: 1,
+  title: "여행 모집",
+  nickname: "야돈",
+  country: "일본",
+  city: "오사카",
+  startDate: "2024-03-15",
+  endDate: "2024-03-20",
+  status: "모집 중",
+  thumbnail: "/images/logo.svg",
+  countOfComments: 3,
+  countOfBookmarks: 5,
+};
+
 function Card() {
-  const [gather, setGather] = useState(false);
   const [favor, setFavor] = useState(false);
   return (
     <Link
-      href={"/"}
+      href={`/travel/${content.id}/detail`}
       className="tablet:w-196 mobile:max-w-[280px] mobile:min-w-264 mobile:w-full w-240 h-[310px] block bg-white rounded-16 border border-line-02 m-auto"
     >
       <div className="w-full h-180 p-16 tablet:p-12 flex flex-col rounded-16 relative overflow-hidden border">
         <Image
-          src={"/images/logo.svg"}
+          src={content.thumbnail}
           alt="여행지 이미지"
           fill
           className="z-0 object-cover"
@@ -20,7 +33,7 @@ function Card() {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-20"></div>
         <div className="relative w-full z-1">
           <div className="flex justify-between">
-            {gather ? (
+            {content.status === "모집 완료" ? (
               <div className="px-12 py-5 w-max tablet:py-3 tablet:px-10 bg-stone-100 rounded-24 text-14 tablet:text-12 text-stone-500">
                 모집 완료
               </div>
@@ -41,9 +54,9 @@ function Card() {
           </div>
 
           <div className="mt-16 leading-tight text-white text-16 tablet:text-14">
-            길동무 모집글 제목길동무 모집글 제목길동
+            {content.title}
           </div>
-          <div className="mt-1 text-white text-14">작성자</div>
+          <div className="mt-1 text-white text-14">{content.nickname}</div>
         </div>
       </div>
 
@@ -52,14 +65,18 @@ function Card() {
           <div className="relative w-16 h-16 tablet:w-12 tablet:h-12">
             <Image src={"/icons/location.svg"} alt="위치" fill />
           </div>
-          <div>한국, 제주특별자치도</div>
+          <div>
+            {content.country}, {content.city}
+          </div>
         </div>
 
         <div className="flex items-center flex-1 gap-8">
           <div className="relative w-16 h-16 tablet:w-12 tablet:h-12">
             <Image src={"/icons/calendar.svg"} alt="일정" fill />
           </div>
-          <div>2024/03/10-2024/03/14</div>
+          <div>
+            {content.startDate} ~ {content.endDate}
+          </div>
         </div>
 
         <div className="flex items-center flex-1 gap-8">
@@ -74,14 +91,14 @@ function Card() {
             <div className="relative w-12 h-12">
               <Image src={"/icons/heart.svg"} alt="좋아요 수" fill />
             </div>
-            <div>00</div>
+            <div>{content.countOfBookmarks}</div>
           </div>
 
           <div className="flex items-center gap-4">
             <div className="relative w-12 h-12">
               <Image src={"/icons/comment.svg"} alt="댓글 수" fill />
             </div>
-            <div>00</div>
+            <div>{content.countOfComments}</div>
           </div>
         </div>
       </div>
