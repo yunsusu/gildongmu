0ae2b9f8ced193cfe32a7b38fa93af091045ee2e
JunCastle <juncastle97@gmail.com>
2024-03-15 16:12:45 +0900

    fix : TagInput 컴포넌트 공통 컴포넌트로 수정

--- components/form/input/TagInput.tsx
+++ components/form/input/TagInput.tsx
@@ -3,7 +3,7 @@ import React, { KeyboardEvent, useEffect, useState } from "react";
 
 import { Input } from "@/components/ui/input";
 
-function TagInput({ onChange, value }: any) {
+function TagInput({ onChange, value, formType }: any) {
   const [tags, setTags] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState("");
 
@@ -19,7 +19,7 @@ function TagInput({ onChange, value }: any) {
 
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      event.preventDefault(); // 폼 제출 방지
+      event.preventDefault();
       if (!event.nativeEvent.isComposing) {
         const newTag = inputValue.trim();
         if (newTag && !tags.includes(newTag) && tags.length < 3) {
@@ -43,6 +43,24 @@ function TagInput({ onChange, value }: any) {
     "text-tag-pink-500",
   ];
 
+  let placeholderText = "";
+  switch (formType) {
+    case "write":
+      placeholderText =
+        tags.length < 3
+          ? "#태그를 입력해 주세요"
+          : "태그는 최대 3개까지만 가능합니다";
+      break;
+    case "signUp":
+      placeholderText =
+        tags.length < 3
+          ? "좋아하는 여행지를 입력해 주세요"
+          : "태그는 최대 3개까지만 가능합니다";
+      break;
+    default:
+      break;
+  }
+
   return (
     <div>
       <Input
@@ -53,11 +71,7 @@ function TagInput({ onChange, value }: any) {
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
         maxLength={20}
-        placeholder={
-          tags.length < 3
-            ? "좋아하는 여행지 입력 후 엔터"
-            : "태그는 최대 3개까지만 가능합니다"
-        }
+        placeholder={placeholderText}
       />
       <div className="flex flex-wrap gap-5">
         {tags.map((tag, index) => (
