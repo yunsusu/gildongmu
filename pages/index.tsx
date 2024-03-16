507807df5e0f896c19ac412295c42efa10df147c
imkrmin <zum2n2@gmail.com>
2024-03-16 19:42:57 +0900

    chore : index 초기화

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import WritingButton from "@/components/button/writing";
 import Header from "@/components/header";
 import Contents from "@/components/main/contents";
 import Footer from "@/components/main/footer";
-import Modal from "@/components/modal";
 
 export default function Main() {
   return (
@@ -10,7 +9,6 @@ export default function Main() {
       <Header headerColor="bg-primary-press">
         이번 여행, 어디로 떠나볼까요?
       </Header>
-      <Modal modalType="userProfile" />
       <Contents />
       <WritingButton />
       <Footer />
