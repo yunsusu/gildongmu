998d9a16a7828caca9f88f4b3f9810e831eeb9ed
bumsly <qjatmd2005@naver.com>
2024-03-16 19:02:44 +0900

    feat : 로그인 구현 추가 & 회원가입 링크 넣기

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import useToggle from "@/hooks/useToggle";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const [eye, setEye, toggleEye] = useToggle(true);
+
+  const handleLogin = async (event: any) => {
+    event?.preventDefault();
+
+    try {
+      const response = await fetch("http://3.38.76.39:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("로그인 실패");
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
@@ -13,18 +38,22 @@ export default function Login() {
         <div className="flex h-full w-1/2 items-center justify-center bg-bg-06 text-14 tablet:w-full">
           <div className="flex h-5/6 max-h-[617px] w-[434px] flex-col items-center justify-between rounded-32 bg-white p-40 tablet:mt-[81.5px] mobile:mx-24 mobile:mt-[50.5px] mobile:w-full">
             <h1 className="text-32 font-extrabold text-text-01">로그인</h1>
-            <ul className="w-full">
-              <li className="relative mb-32 w-full">
+            <form onSubmit={handleLogin} className="w-full">
+              <div className="relative mb-32 w-full">
                 <input
                   type="email"
                   placeholder="이메일"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
                   className="flex h-52 w-full items-center justify-end gap-8 self-stretch rounded-xl bg-bg-02 px-16"
                 />
-              </li>
-              <li className="relative w-full">
+              </div>
+              <div className="relative w-full">
                 <input
                   type={eye ? "password" : "text"}
                   placeholder="비밀번호"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
                   className="flex h-52 w-full items-center justify-end gap-8 self-stretch rounded-xl bg-bg-02 px-16"
                 />
                 <Image
@@ -35,22 +64,23 @@ export default function Login() {
                   className="absolute right-16 top-14"
                   onClick={toggleEye}
                 />
-              </li>
-            </ul>
-            <div className="w-full text-18">
-              <Button
-                variant="ghost"
-                className="mb-20 h-52 w-full bg-stone-200"
-              >
-                로그인
-              </Button>
+              </div>
+              <div className="mt-24 w-full text-18">
+                <Button variant="ghost" className="h-52 w-full bg-stone-200">
+                  로그인
+                </Button>
+              </div>
+            </form>
+
+            <Link href="/signup" className="w-full text-18">
               <Button
                 variant="ghost"
                 className="h-52 w-full border-[1.5px] !text-primary hover:bg-primary-press hover:!text-primary-foreground"
               >
                 회원 가입하기
               </Button>
-            </div>
+            </Link>
+
             <div className="flex w-full items-center justify-center">
               <div className="w-1/3 border-b border-line-01"></div>
               <p className="flex w-1/3 justify-center text-16 leading-[130%] tracking-[-0.6px] text-text-01">
