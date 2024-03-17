575314ca4532705a412b002e01a79853c406ed57
eonpain <zmfltmwnsrl@naver.com>
2024-03-17 23:29:56 +0900

    feat: 상단의 모집정보, 여행지, 댓글 nav가 스크롤을 내려도 상단에 고정되도록 클래스속성 추가

--- pages/travel/[Id]/detail/index.tsx
+++ pages/travel/[Id]/detail/index.tsx
@@ -67,7 +67,7 @@ function Content() {
   });
   return (
     <div className="flex w-[956px] flex-col items-center">
-      <div className="flex w-full items-start gap-4 text-20 font-bold text-sky-600">
+      <div className="sticky top-0 z-50 flex w-full items-start gap-4 text-20 font-bold text-sky-600">
         <a
           className="flex h-60 w-1/3 justify-around rounded-tl-32 rounded-tr-32 border-none"
           href="#information"
