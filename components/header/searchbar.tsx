0b826db4d0cb992efe4c263fa4b07d3bd1db4de8
imkrmin <zum2n2@gmail.com>
2024-03-19 18:07:21 +0900

    design : 서치바 넓이 수정 및 placeholder 텍스트 크기 수정

--- components/header/searchbar.tsx
+++ components/header/searchbar.tsx
@@ -4,15 +4,15 @@ import { Input } from "@/components/ui/input";
 
 export default function Searchbar() {
   const placeholderStyle =
-    "placeholder:text-18 placeholder:font-bold placeholder:leading-tight placeholder:tracking-tight";
+    "placeholder:text-18 placeholder:font-bold placeholder:leading-tight placeholder:tracking-tight placeholder:tablet:text-16";
 
   return (
     <div className="relative">
       <Input
-        className={`flex w-[564px] tablet:w-[564px] mobile:w-312 h-60 tablet:h-52 pt-12 pb-12 pl-24 pr-12 rounded-[32px] bg-bg-01 tracking-tight leading-tight font-bold text-text-02 text-xl focus-visible:ring-0 focus-visible:ring-offset-0 tablet:text-16 ${placeholderStyle}`}
+        className={`bg-bg-01 flex h-60 w-[564px] rounded-[32px] pb-12 pl-24 pr-12 pt-12 text-xl font-bold leading-tight tracking-tight text-text-02 focus-visible:ring-0 focus-visible:ring-offset-0 tablet:h-52 tablet:w-[534px] tablet:text-16 mobile:w-312 ${placeholderStyle}`}
         placeholder="가고 싶은 여행지를 검색해 주세요!"
       />
-      <button className="absolute top-1/2 -translate-y-1/2 right-20">
+      <button className="absolute right-20 top-1/2 -translate-y-1/2">
         <Image
           src={"/icons/search.svg"}
           alt="돋보기 아이콘"
