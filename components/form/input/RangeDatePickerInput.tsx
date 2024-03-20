60475bff5b02b6a649ee4d63886febd6141edda8
JunCastle <juncastle97@gmail.com>
2024-03-20 22:09:17 +0900

    fix : 인풋 rounded 수정

--- components/form/input/RangeDatePickerInput.tsx
+++ components/form/input/RangeDatePickerInput.tsx
@@ -60,7 +60,7 @@ function RangeDatePickerInput({
           readOnly
           placeholder="여행 일정을 선택해 주세요"
           onClick={() => setIsPickerOpen(true)}
-          className="h-52 w-[756px] rounded-12 border border-line-02 bg-bg-02 px-16 placeholder:text-text-05 focus:border focus:border-line-01 focus:bg-white focus-visible:ring-0 focus-visible:ring-offset-0 tablet:w-[672px] mobile:w-272 mobile:text-sm"
+          className="h-52 w-[756px] rounded-2xl border border-line-02 bg-bg-02 px-16 placeholder:text-text-05 focus:border focus:border-line-01 focus:bg-white focus-visible:ring-0 focus-visible:ring-offset-0 tablet:w-[672px] mobile:w-272 mobile:text-sm"
         />
         <style>{css}</style>
         {isPickerOpen && (
