7423ca5b641658729e6e2c26e83c178f0ee5be7f
JunCastle <juncastle97@gmail.com>
2024-03-20 13:27:02 +0900

    feat : DetailTitle 컴포넌트 추가

--- pages/travel/[Id]/detail/index.tsx
+++ pages/travel/[Id]/detail/index.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 import Article from "@/components/detail/article";
 import Header from "@/components/detail/header/index";
+import DetailTitle from "@/components/detail/title/DetailTitle";
 
 function Detail() {
   return (
     <div className="w-1200 flex flex-col bg-sky-50">
       <Header />
+      <DetailTitle />
       <Article />
       <div
         className="fixed bottom-40 right-40 h-64 w-64 animate-bounce cursor-pointer"
