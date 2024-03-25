8cb65d3cf508f47c64764c70d7ca4a92b938187d
imkrmin <zum2n2@gmail.com>
2024-03-25 14:17:49 +0900

    refactor : 찜 버튼 클릭 시 모달이 뜨지 않게 수정

--- components/mytravel/Card/Favor.tsx
+++ components/mytravel/Card/Favor.tsx
@@ -9,14 +9,20 @@ export default function Favor() {
       {favor ? (
         <div
           className="relative h-24 w-24 cursor-pointer"
-          onClick={() => setFavor(false)}
+          onClick={e => {
+            e.stopPropagation();
+            setFavor(false);
+          }}
         >
           <Image src={"/icons/heartOn.svg"} alt="하트 아이콘" fill />
         </div>
       ) : (
         <div
           className="relative h-24 w-24 cursor-pointer"
-          onClick={() => setFavor(true)}
+          onClick={e => {
+            e.stopPropagation();
+            setFavor(true);
+          }}
         >
           <Image src={"/icons/heartOff.svg"} alt="빈하트 아이콘" fill />
         </div>
