0816ea3a70dcb8c698c69542c57cf61c4a57060c
yunsusu <rladbstn1212@gmail.com>
2024-03-26 15:59:50 +0900

    feat : 소통공간 ui 제작

---
+++ components/community/header/index.tsx
@@ -0,0 +1,8 @@
+function CommuHeader() {
+  return (
+    <div className="flex h-120 w-full items-center justify-center text-32 font-extrabold text-text-01 tablet:h-100 tablet:text-24 mobile:h-80">
+      소통공간
+    </div>
+  );
+}
+export default CommuHeader;
