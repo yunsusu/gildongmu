d00e92cf65026ebce0f0fbe60b410216c041c0a1
yunsusu <rladbstn1212@gmail.com>
2024-03-26 16:02:34 +0900

    feat : 소통공간 ui 제작

--- components/community/commulist/index.tsx
+++ components/community/commulist/index.tsx
@@ -4,7 +4,7 @@ function CommuList() {
   return (
     <div
       style={{ height: "calc(100% - 120px)" }}
-      className="h-full w-full px-40 pb-40 tablet:px-20 tablet:pb-20"
+      className="m-auto h-full w-full max-w-[1200px] px-40 pb-40 tablet:px-20 tablet:pb-20"
     >
       <div className="border-Dimensions-05 h-full w-full overflow-scroll rounded-32 border-2 bg-white pt-40 tablet:pt-20">
         <CommuChat />
