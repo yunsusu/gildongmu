5a3e2f0faaa4324b46206edeeeb28377e2d0da1c
yunsusu <rladbstn1212@gmail.com>
2024-04-03 13:12:50 +0900

    fix : 랜딩페이지 하단 슬라이드 정렬 수정

--- components/main/contents/hot.tsx
+++ components/main/contents/hot.tsx
@@ -1,14 +1,14 @@
 import CountryCarousel from "@/components/carousel/country";
 
-export default function Domestic() {
-  const titleIcon = "/icons/car.svg";
+export default function Hot() {
+  const titleIcon = "/icons/icon_hot.svg";
 
   return (
     <div className="flex flex-col bg-white">
       <div className="m-auto flex w-full max-w-[1200px] flex-col gap-40 self-stretch px-24 py-60 tablet:py-40">
         <div className="flex w-full items-center justify-between">
           <div className="flex w-full items-center justify-center">
-            <CountryCarousel titleIcon={titleIcon}>국내 여행</CountryCarousel>
+            <CountryCarousel titleIcon={titleIcon}>HOT</CountryCarousel>
           </div>
         </div>
       </div>
