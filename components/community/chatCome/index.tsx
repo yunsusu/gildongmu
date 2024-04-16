49ca626f3aa7fbb47bff5fd1f4d083ea43be69e5
JunCastle <juncastle97@gmail.com>
2024-04-16 20:51:45 +0900

    chore : 캐시 삭제

--- components/community/chatCome/index.tsx
+++
@@ -1,8 +0,0 @@
-interface ChatProps {
-  text: string;
-}
-function ChatCome({ text }: ChatProps) {
-  return <div className="text-14 text-text-03">{text}</div>;
-}
-
-export default ChatCome;
--- components/community/chatDate/index.tsx
+++
@@ -1,8 +0,0 @@
-interface ChatProps {
-  text: string;
-}
-function ChatDate({ text }: ChatProps) {
-  return <div className="text-14 text-text-04">{text}</div>;
-}
-
-export default ChatDate;
